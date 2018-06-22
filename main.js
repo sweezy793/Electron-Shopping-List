6581dcca1298b822a879210de204dc8f6aa4dbce
sweezy793 <sarthaks793@gmail.com>
2018-06-23 00:39:39 +0530

    Added devtools and now  main window can close add window

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path=require('path');
 const {app,BrowserWindow,Menu}=electron;
 
 let mainWindow;
+let addWindow;
 
 //Listen for app to be ready
 app.on('ready',function(){
@@ -17,11 +18,36 @@ mainWindow=new BrowserWindow({});
         slashes:true
     }));
 
+    //Quit app when closed
+    mainWindow.on('closed',function(){
+        app.quit();
+    })
+
     //Build menu from template
     const mainMenu=Menu.buildFromTemplate(mainMenuTemplate);
     //Insert Menu
     Menu.setApplicationMenu(mainMenu);
 });
+
+//Handle create add window
+function createAddWindow(){
+    addWindow=new BrowserWindow({
+        width:500,
+        height:300,
+        title:'Add Shopping List Item'
+    });
+    //Load html into window
+    addWindow.loadURL(url.format({
+        pathname:path.join(__dirname,'addWindow.html'),
+        protocol:'file:',
+        slashes:true
+    }));
+    //Garbage collection handle
+    addWindow.on('close',function(){
+        addWindow=null;
+    });
+
+}
 
 //Create menu template
 const mainMenuTemplate=[
@@ -29,7 +55,10 @@ const mainMenuTemplate=[
         label:'File',
         submenu:[
             {
-                label:'Add Item'
+                label:'Add Item',
+                click(){
+                    createAddWindow();
+                }
             },
             {
                 label:'Clear Items'
@@ -44,3 +73,24 @@ const mainMenuTemplate=[
         ]
     }
 ];
+
+//Developer tools
+if(process.env.NODE_ENV!=='production')
+{
+    mainMenuTemplate.push({
+        label:'Developer Tools',
+        submenu:[
+            {
+                label:'Toggle Dev Tools',
+                accelerator:'CmdOrCtrl+I',
+                click(item,focusedWindow)
+                {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role:'reload'
+            }
+        ]
+    });
+}
