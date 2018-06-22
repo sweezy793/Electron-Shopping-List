d73c477247676a8b61d3fb351e903f62b9498f33
sweezy793 <sarthaks793@gmail.com>
2018-06-22 23:55:45 +0530

    Replaced default menu

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron =require('electron');
 const url=require('url');
 const path=require('path');
 
-const {app,BrowserWindow}=electron;
+const {app,BrowserWindow,Menu}=electron;
 
 let mainWindow;
 
@@ -16,4 +16,31 @@ mainWindow=new BrowserWindow({});
         protocol:'file:',
         slashes:true
     }));
+
+    //Build menu from template
+    const mainMenu=Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert Menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+//Create menu template
+const mainMenuTemplate=[
+    {
+        label:'File',
+        submenu:[
+            {
+                label:'Add Item'
+            },
+            {
+                label:'Clear Items'
+            },
+            {
+                label:'Quit',
+                accelerator:'CmdOrCtrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
