d7915b5b8b11b0e487c5548f67cb515e1dc86ece
sweezy793 <sarthaks793@gmail.com>
2018-06-24 23:51:56 +0530

    Item added shows up on terminal

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron =require('electron');
 const url=require('url');
 const path=require('path');
 
-const {app,BrowserWindow,Menu}=electron;
+const {app,BrowserWindow,Menu,ipcMain}=electron;
 
 let mainWindow;
 let addWindow;
@@ -48,6 +48,13 @@ function createAddWindow(){
     });
 
 }
+
+//Catch item:add
+ipcMain.on('item:add',function(e,item){
+    console.log(item);
+    mainWindow.webContents.send('item:add',item);
+    addWindow.close();
+});
 
 //Create menu template
 const mainMenuTemplate=[
